4a6e5758a87258da023155c0cf5551550fb6c4ef
AminPooja15 <109980519+AminPooja15@users.noreply.github.com>
2022-08-27 18:41:04 -0400

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csv
+{
+    public class Person
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public int Version { get; set; }
+        public string InsuranceCompany { get; set; } = string.Empty;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using csv;
+using System.Text;
+
+Console.WriteLine("Enter File Path for CSV File.");
+string? filePath = Console.ReadLine();
+
+Console.WriteLine("Enter Write Path (Folder)");
+string? writePath = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(filePath))
+{
+    List<Person> data = new List<Person>();
+    FileStream fileStream = new FileStream(filePath, FileMode.Open);
+    using StreamReader reader = new StreamReader(fileStream);
+
+    string? line;
+    string? header = reader.ReadLine();
+    while ((line = reader.ReadLine()) != null)
+    {
+        if (!string.IsNullOrEmpty(line))
+        {
+            string [] csv = line.Trim().Split(",");
+
+            Person person = new Person
+            {
+                UserId = csv[0].Trim(),
+                Name = csv[1].Trim(),
+                Version = int.Parse(csv[2]),
+                InsuranceCompany = csv[3].Trim()
+            };
+
+            data.Add(person);
+        }
+
+        var dataByInsurance = data.GroupBy(x => x.InsuranceCompany);
+
+        foreach (var d in dataByInsurance)
+        {
+            var orderedData = d.OrderBy(x => x.Name).ToList();
+
+            using StreamWriter file = new StreamWriter(File.Create($"{writePath}/{d.Key}.csv"));
+
+            StringBuilder sb = new StringBuilder("User Id, Name, Version, Insurance Company \n");
+
+            orderedData.ForEach(x => sb.AppendLine($"{x.UserId}, {x.Name}, {x.Version}, {x.InsuranceCompany}"));
+
+            file.WriteLine(sb.ToString());
+        }
+    }
+}
